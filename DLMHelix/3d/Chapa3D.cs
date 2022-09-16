70b31aa5ca053f5915c76f15e40fbd042c4835eb
Daniel Lins Maciel <dan.8p@hotmail.com>
2022-09-16 16:45:37 -0300

    Ajustes

--- DLMHelix/3d/Chapa3D.cs
+++ DLMHelix/3d/Chapa3D.cs
@@ -297,7 +297,7 @@ namespace DLM.helix
         {
             this.Espessura = Espessura;
             this.Pontos.AddRange(face.LivSegmentada.Select(x => new P3d(x.X, x.Y, 0)));
-            this.Aberturas.AddRange(face.Furacoes.FindAll(x => x.MinX > face.MinX && x.MaxX < face.MaxX).Select(y => new DLM.helix.Abertura3d(y.Diametro, y.X, y.Y, y.Dist, y.Ang)));
+            this.Aberturas.AddRange(face.Furacoes.GroupBy(x=> x.ToString()).Select(x=>x.First()).ToList().FindAll(x => x.MinX > face.MinX && x.MaxX < face.MaxX).Select(y => new DLM.helix.Abertura3d(y.Diametro, y.X, y.Y, y.Dist, y.Ang)));
             this.Aberturas.AddRange(face.RecortesInternos.Select(x => new Abertura3d(x.GetLiv(2))));
             this.Cor = Brushes.Green.Clone();
         }

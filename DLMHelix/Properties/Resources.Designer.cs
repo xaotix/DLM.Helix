876140d52afa9048dcaef892e4b35bfc97a56590
Daniel Lins Maciel <dan.8p@hotmail.com>
2023-02-03 14:54:19 -0300

    Net 4.8

--- DLMHelix/Properties/Resources.Designer.cs
+++ DLMHelix/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DLM.helix.Properties {
+namespace DLMHelix.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DLM.helix.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DLM.helix.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DLMHelix.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

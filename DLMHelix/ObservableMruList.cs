e06b58b59759bed5c9d64d1bcbc7500208c8e3c2
Daniel Lins Maciel <dan.8p@hotmail.com>
2023-06-05 07:52:14 -0300

    Removido visualizador de IFC da DLL base e colocado em DLM.helix

---
+++ DLMHelix/ObservableMruList.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace DLM.helix
+{
+    public class ObservableMruList<T> : ObservableCollection<T>
+    {
+        private readonly int _maxSize = -1;
+        private readonly IEqualityComparer<T> _itemComparer;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ObservableMruList()
+        { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="collection"></param>
+        public ObservableMruList(IEnumerable<T> collection)
+            : base(collection)
+        { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="list"></param>
+        public ObservableMruList(List<T> list)
+            : base(list)
+        { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="maxSize"></param>
+        /// <param name="itemComparer"></param>
+        public ObservableMruList(int maxSize, IEqualityComparer<T> itemComparer)
+        {
+            _maxSize = maxSize;
+            _itemComparer = itemComparer;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="maxSize"></param>
+        /// <param name="itemComparer"></param>
+        public ObservableMruList(IEnumerable<T> collection, int maxSize, IEqualityComparer<T> itemComparer)
+            : base(collection)
+        {
+            _maxSize = maxSize;
+            _itemComparer = itemComparer;
+            RemoveOverflow();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="maxSize"></param>
+        /// <param name="itemComparer"></param>
+        public ObservableMruList(List<T> list, int maxSize, IEqualityComparer<T> itemComparer)
+            : base(list)
+        {
+            _maxSize = maxSize;
+            _itemComparer = itemComparer;
+            RemoveOverflow();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MaxSize
+        {
+            get { return _maxSize; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        public new void Add(T item)
+        {
+            int indexOfMatch = IndexOf(item);
+            if (indexOfMatch < 0)
+            {
+                Insert(0, item);
+            }
+            else
+            {
+                Move(indexOfMatch, 0);
+            }
+            RemoveOverflow();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public new bool Contains(T item)
+        {
+            return this.Contains(item, _itemComparer);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public new int IndexOf(T item)
+        {
+            int indexOfMatch = -1;
+            if (_itemComparer != null)
+            {
+                for (int idx = 0; idx < Count; idx++)
+                {
+                    if (_itemComparer.Equals(item, this[idx]))
+                    {
+                        indexOfMatch = idx; break;
+                    }
+                }
+            }
+            else
+            {
+                indexOfMatch = base.IndexOf(item);
+            }
+            return indexOfMatch;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public new bool Remove(T item)
+        {
+            bool opResult = false;
+
+            int targetIndex = IndexOf(item);
+            if (targetIndex > -1)
+            {
+                RemoveAt(targetIndex);
+                opResult = true;
+            }
+
+            return opResult;
+        }
+
+        private void RemoveOverflow()
+        {
+            if (MaxSize > 0)
+            {
+                while (Count > MaxSize)
+                {
+                    RemoveAt(Count - 1);
+                }
+            }
+        }
+    }
+}

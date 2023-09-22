c6b04a992d0d81982792744d93e19bbdc098b667
Daniel Lins Maciel <dan.8p@hotmail.com>
2023-09-22 14:33:58 -0300

    Update Extensoes.cs

--- DLMHelix/Extensoes.cs
+++ DLMHelix/Extensoes.cs
@@ -1,4 +1,5 @@
 ﻿using Conexoes;
+using DLM.cam;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,14 @@ namespace DLM.Helix
 {
     public static class Extensoes
     {
-        public static void Open(this netDxf.DxfDocument Arquivo)
+        public static void Show(this netDxf.DxfDocument Arquivo)
+        {
+            var mm = new OpenViewer();
+            mm.Show();
+            //mm.Title = Arquivo.Name;
+            mm.Viewer.Abrir(Arquivo);
+        }
+        public static void Show(this ReadCAM Arquivo)
         {
             var mm = new OpenViewer();
             mm.Show();

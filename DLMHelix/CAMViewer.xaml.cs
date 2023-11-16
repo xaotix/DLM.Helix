a02264630e30089f4ffaa9cd56975562c6a0b24b
Daniel Lins Maciel <dan.8p@hotmail.com>
2023-11-16 13:48:52 -0300

    Ajustes

--- DLMHelix/CAMViewer.xaml.cs
+++ DLMHelix/CAMViewer.xaml.cs
@@ -28,7 +28,7 @@ namespace DLM.helix
         }
         public void Abrir(string arq)
         {
-            this.viewPort.Children.Clear();
+            this.viewPort3D.Children.Clear();
             this.viewPort2D.Children.Clear();
 
             if (!File.Exists(arq))
@@ -60,10 +60,10 @@ namespace DLM.helix
         }
         public void Abrir(ReadCAM arq)
         {
-            this.viewPort.Children.Clear();
+            this.viewPort3D.Children.Clear();
             this.viewPort2D.Children.Clear();
             this.tab_3d.Visibility = Visibility.Visible;
-            this.tab_3d.IsSelected = true;
+            //this.tab_3d.IsSelected = true;
 
             this.MVC.CAM = arq;
             Recarregar();
@@ -78,7 +78,7 @@ namespace DLM.helix
 
         public void Abrir(netDxf.DxfDocument dxfDocument)
         {
-            this.viewPort.Children.Clear();
+            this.viewPort3D.Children.Clear();
             this.viewPort2D.Children.Clear();
             dxfDocument.RenderHelix(this.viewPort2D);
             this.tab_3d.Visibility = Visibility.Collapsed;
@@ -95,42 +95,46 @@ namespace DLM.helix
 
             Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
             {
-                viewPort.Children.Clear();
-                viewPort.Children.Add(Gera3d.Luz());
+                viewPort3D.Children.Clear();
+                viewPort3D.Children.Add(Gera3d.Luz());
                 //var readcam = readcam.GetCam();
-                viewPort.AddUCSIcon(this.MVC.CAM.Formato.Comprimento / 10);
-                viewPort.ShowCameraTarget = true;
+                viewPort3D.AddUCSIcon(this.MVC.CAM.Formato.Comprimento / 10);
+                viewPort3D.ShowCameraTarget = true;
 
                 //var chapas3d = Gera3d.Desenho(this.MVC.CAM);
                 var chapas3d = this.MVC.CAM.GetChapas3Ds();
-                List<Rect3D> recs = new List<Rect3D>();
-                Bounds = null;
+                var recs = new List<Rect3D>();
                 foreach (var chapa in chapas3d)
                 {
                     var desenho = chapa.GetDesenho3D();
 
                     var rec = desenho.FindBounds(desenho.Transform);
                     recs.Add(rec);
-                    viewPort.Children.Add(desenho);
-                }
-                Render2d.RenderHelix(this.MVC.CAM, this.viewPort2D);
-                this.viewPort.Children.Add(DLM.helix.Gera3d.Luz());
-
-                if (recs.Count > 0)
-                {
-                    List<P3d> pts = recs.Select(x => new P3d(x.X, x.Y, x.Z, false)).ToList();
-                    var p1 = pts.Min();
-                    var p2 = pts.Max();
-
-                    Bounds = new Rect3D(p1.X, p1.Y, p1.Z, p1.DistanciaX(p2).Abs(), p1.DistanciaY(p2).Abs(), p1.DistanciaZ(p2).Abs());
+                    viewPort3D.Children.Add(desenho);
                 }
 
+                var dxf = this.MVC.CAM.Formato.GetDxf();
+                dxf.RenderHelix(this.viewPort2D);
 
-                this.viewPort.ShowCoordinateSystem = true;
-                this.viewPort.ShowFieldOfView = false;
-                this.viewPort.ShowViewCube = true;
-                this.viewPort.ShowCameraTarget = false;
-                this.viewPort.ShowCameraInfo = false;
+                //Bounds = null;
+                //Render2d.RenderHelix(this.MVC.CAM, this.viewPort2D);
+                //this.viewPort.Children.Add(DLM.helix.Gera3d.Luz());
+
+                //if (recs.Count > 0)
+                //{
+                //    List<P3d> pts = recs.Select(x => new P3d(x.X, x.Y, x.Z, false)).ToList();
+                //    var p1 = pts.Min();
+                //    var p2 = pts.Max();
+
+                //    Bounds = new Rect3D(p1.X, p1.Y, p1.Z, p1.DistanciaX(p2).Abs(), p1.DistanciaY(p2).Abs(), p1.DistanciaZ(p2).Abs());
+                //}
+
+
+                this.viewPort3D.ShowCoordinateSystem = true;
+                this.viewPort3D.ShowFieldOfView = false;
+                this.viewPort3D.ShowViewCube = true;
+                this.viewPort3D.ShowCameraTarget = false;
+                this.viewPort3D.ShowCameraInfo = false;
 
                 this.viewPort2D.ShowCoordinateSystem = false;
                 this.viewPort2D.ShowFieldOfView = false;
@@ -158,7 +162,7 @@ namespace DLM.helix
 
         private void Front()
         {
-            ControleCamera.Setar(this.viewPort, ControleCamera.eCameraViews.Top, 0);
+            ControleCamera.Setar(this.viewPort3D, ControleCamera.eCameraViews.Top, 0);
             ControleCamera.Setar(viewPort2D, ControleCamera.eCameraViews.Right, 0);
 
         }
@@ -180,25 +184,25 @@ namespace DLM.helix
 
         private void top(object sender, RoutedEventArgs e)
         {
-            ControleCamera.Setar(this.viewPort, ControleCamera.eCameraViews.Right, 0);
+            ControleCamera.Setar(this.viewPort3D, ControleCamera.eCameraViews.Right, 0);
             SetView2D();
         }
 
         private void bottom(object sender, RoutedEventArgs e)
         {
-            ControleCamera.Setar(this.viewPort, ControleCamera.eCameraViews.Left, 0);
+            ControleCamera.Setar(this.viewPort3D, ControleCamera.eCameraViews.Left, 0);
             SetView2D();
         }
 
         private void left(object sender, RoutedEventArgs e)
         {
-            ControleCamera.Setar(this.viewPort, ControleCamera.eCameraViews.Back, 0);
+            ControleCamera.Setar(this.viewPort3D, ControleCamera.eCameraViews.Back, 0);
             SetView2D();
         }
 
         private void right(object sender, RoutedEventArgs e)
         {
-            ControleCamera.Setar(this.viewPort, ControleCamera.eCameraViews.Bottom, 0);
+            ControleCamera.Setar(this.viewPort3D, ControleCamera.eCameraViews.Bottom, 0);
             SetView2D();
 
         }
@@ -211,7 +215,7 @@ namespace DLM.helix
         public void ZoomExtend()
         {
 
-            this.viewPort.ZoomExtents();
+            this.viewPort3D.ZoomExtents();
             this.viewPort2D.ZoomExtents();
         }
 
@@ -223,7 +227,7 @@ namespace DLM.helix
 
         public void Isometric()
         {
-            ControleCamera.Setar(this.viewPort, ControleCamera.eCameraViews.Isometric_PPP, 0);
+            ControleCamera.Setar(this.viewPort3D, ControleCamera.eCameraViews.Isometric_PPP, 0);
         }
 
         private void abrir(object sender, RoutedEventArgs e)
